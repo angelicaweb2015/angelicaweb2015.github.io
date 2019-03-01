a79ec9c679c9dfd2d745666845f9e16ec71a3abb
angelicaweb2015 <angelicaespinoza112@gmail.com>
2019-02-28 22:16:04 -0600

    second commit, actually doing stuff lol marco was here

---
+++ contact.php
@@ -0,0 +1,230 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+
+	<!--Google AdSense-->
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-2058784414719537",
+    enable_page_level_ads: true
+  });
+</script>
+	
+	<!--Google Optimize-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-70050928-1', 'auto');
+  ga('require', 'GTM-KFK4Q65');
+  ga('send', 'pageview');
+</script>
+<!--Step 2: Minimize page flickering
+This snippet ensures that users only see your variant content, minimizing page flicker. Learn more
+Copy the code below-->
+<style>.async-hide { opacity: 0 !important} </style>
+<script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+})(window,document.documentElement,'async-hide','dataLayer',4000,
+{'GTM-KFK4Q65':true});</script>
+<!--Paste it just before your updated Analytics tracking code anywhere you want to use Optimize.-->
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-70050928-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-70050928-1');
+
+</script>
+
+		<title>Contact | Angelica Espinoza</title>
+		<meta charset="utf-8" />
+				<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="I am a web developer and graphic designer based in San Antonio, Texas. I am passionate about building the digital infrastructure to showcase new and small businesses. My passion is building a dynamic web presence for small business owners to get their products and services recognized." />
+		<meta name="keywords" content="angelica,espinoza,web,developer,San Antonio,graphic designer,logo,logos,create logos,brand,brands,branding,marketing,advertising,print ads,digital ads,advertise,social media,facebook,twitter,instagram,snapchat,linkedin,websites,wordpress,bluehost,wix,elementor,squarespace,local web designer in San Antonio,local graphic designer in San Antonio,local logo maker in San Antonio,local print ad designer in San Antonio,local digital ad designer in San Antonio" />
+		<meta name="robots" content="index,follow" />
+		<meta name="referrer" content="origin" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<h1><a href="index.html">Angelica</a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Menu</span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="/">Home</a></li>
+											<!--<li><a href="casestudies.html">Case Studies</a></li>-->
+											<li><a href="services">Services</a></li>
+											<!--<li><a href="clienttestimonialform.html">Client Testimonial Form</a></li>-->
+											<li><a href="resources">Resources</a></li>
+											<li><a href="contact.php">Contact</a></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Main -->
+					<article id="main">
+						<header>
+							<h2>Contact Me</h2>
+							<!--<p>Aliquam ut ex ut interdum donec amet imperdiet eleifend</p>-->
+						</header>
+						<section class="wrapper style5">
+							<div class="inner">
+
+								<!--<h3><center>Coming soon.</center></h3>-->
+
+<form action="https://formspree.io/hello@aecreativeworks.com"
+      method="POST">
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;}
+
+input[type=text], select, textarea {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    margin-top: 6px;
+    margin-bottom: 16px;
+    resize: vertical;
+}
+
+input[type=submit] {
+    background-color: 	#008000;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+input[type=submit]:hover {
+    background-color: #blue;
+}
+
+.container {
+    border-radius: 5px;
+    background-color: ##f7f4f4;
+    padding: 20px;
+}
+</style>
+</head>
+<body>
+
+<h3>Contact Form</h3>
+
+<fieldset>
+<div class="container">
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="First Name" required placeholder="Your first name...">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="Last Name" required placeholder="Your last name...">
+
+    <label for="email">E-mail</label>
+    <input type="text" id="email" name="E mail" required placeholder="Your e-mail...">
+
+    <label for="phonenumber">Phone Number</label>
+    <input type="text" id="email" name="Phone Number" required placeholder="Your phone number...">
+
+   <label required for="servicetype">Which of the following services are you interested in knowing more about?</label>
+    <div>
+      <input type="checkbox" id="logos" name="Interest" value="logos">
+      <label for="logos">Logos</label>
+    </div>
+    <div>
+      <input type="checkbox" id="websites" name="Interest" value="websites">
+      <label for="websites">Websites</label>
+    </div>
+    <div>
+      <input type="checkbox" id="ecommerce" name="Interest" value="ecommerce">
+      <label for="ecommerce">E-Commerce Website</label>
+    </div>
+    <div>
+      <input type="checkbox" id="printads" name="Interest" value="printads">
+      <label for="printads">Print Ads</label>
+    </div>
+    <div>
+      <input type="checkbox" id="digitalads" name="Interest" value="digitalads">
+      <label for="digitalads">Digital Ads</label>
+    </div>
+    <div>
+      <input type="checkbox" id="advertising" name="Interest" value="advertising">
+      <label for="advertising">Advertising</label>
+    </div>
+    <div>
+
+   <label for="subject">Additional Request</label>
+    <textarea id="subject" name="Additional Request" placeholder="Type request here..." style="height:200px"></textarea>
+    
+    <input type="submit" value="Send">
+
+</fieldset>
+  </form>
+
+<!-- 
+<input type="hidden" name="email" value="https://formspree.io/hello@aecreativeworks.com">
+<input type="hidden" name="email" value="90d8a0f9d87a0987d098ad87fa0ds7fd">
+<input type="hidden" name="product" value="0870n89f7cxz98f7e0qwd87sdf"> -->
+
+
+</div>
+</section>
+</body>
+							        				
+				<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+
+						<li><a href="https://www.facebook.com/AECreativeWorks/" target="_blank"class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+
+						<li><a href="https://www.instagram.com/aecreativeworks/" target="_blank" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+
+						<li><a href="https://twitter.com/AECreativeWorks" target="_blank" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+
+						<li><a href="https://www.linkedin.com/company/ae-creative-works/" target="_blank" class="icon alt fa-linkedin"><span class="label">LinkedIn
+						</span></a></li>
+
+					</ul>
+					<ul class="copyright">
+						<li>Website created by: AE Creative Works &trade;</li> <li>Web Designer: Angelica Espinoza &copy;</li>
+					</ul>
+				</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
